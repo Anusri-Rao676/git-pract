3c5e9047f310c07c61fc62196fe23d854b87298f
Anusri Rao <anusriraok@gmail.com>
2025-09-09 12:26:28 +0530

    reduced coverage

--- test-calc.py
+++ test-calc.py
@@ -3,14 +3,15 @@ from calc import calculator
 
 mycalc = calculator(10,5)
 class test_calc(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(mycalc.add(),15)
+    # def test_add(self):
+    #     self.assertEqual(mycalc.add(),15)
     # def test_subtract(self):
     #     self.assertEqual(mycalc.subtract(),5)
     # def test_multiply(self):
     #     self.assertEqual(mycalc.multiply(),50)
     # def test_divide(self):
     #     self.assertEqual(mycalc.divide(),2)
+    pass
 
 if __name__ == "__main__":
     unittest.main()    
