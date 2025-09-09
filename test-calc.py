0e236a3fbf2d3a45c486dae128cf1bc6797fa8fe
Anusri Rao <anusriraok@gmail.com>
2025-09-09 12:21:54 +0530

    Update test-calc.py

--- test-calc.py
+++ test-calc.py
@@ -5,8 +5,8 @@ mycalc = calculator(10,5)
 class test_calc(unittest.TestCase):
     def test_add(self):
         self.assertEqual(mycalc.add(),15)
-    def test_subtract(self):
-        self.assertEqual(mycalc.subtract(),5)
+    # def test_subtract(self):
+    #     self.assertEqual(mycalc.subtract(),5)
     # def test_multiply(self):
     #     self.assertEqual(mycalc.multiply(),50)
     # def test_divide(self):
