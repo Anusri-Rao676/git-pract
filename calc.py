0295244c445d6e6bf4548ec91534c1b5ba2ff3e2
Anusri Rao <anusriraok@gmail.com>
2025-09-09 12:29:30 +0530

    Update calc.py

--- calc.py
+++ calc.py
@@ -7,8 +7,8 @@ class calculator:
         return self.num1 + self.num2
     def subtract(self):
         return self.num1 - self.num2
-    # def multiply(self):
-    #     return self.num1 * self.num2
-    # def divide(self):
-    #     return self.num1/self.num2
+    def multiply(self):
+        return self.num1 * self.num2
+    def divide(self):
+        return self.num1/self.num2
     
